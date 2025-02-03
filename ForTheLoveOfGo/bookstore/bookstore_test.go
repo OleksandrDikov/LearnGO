b4d391ae5ee1fea564470f4b262d310280fd5114
OleksandrDikov <o.dikov@luxnet.io>
2025-02-03 18:19:03 +0200

    complete ch 5

--- ForTheLoveOfGo/bookstore/bookstore_test.go
+++ ForTheLoveOfGo/bookstore/bookstore_test.go
@@ -5,18 +5,42 @@ import (
 	"testing"
 )
 
-//
-//bookstore.Book {
-//	Title: "The Art of Computer Programming",
-//	Author: "Donald Knuth",
-//	Copies: 3,
-//}
-
 func TestBook(t *testing.T) {
 	t.Parallel()
 	_ = bookstore.Book{
 		Title:  "Spark Joy",
 		Author: "Marie Kondo",
 		Copies: 2,
+	}
+}
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 2,
+	}
+	result, err := bookstore.Buy(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := result.Copies
+	want := 1
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+        Title:  "Spark Joy",
+        Author: "Marie Kondo",
+        Copies: 0,
+    }
+	_, err := bookstore.Buy(b)
+	if err == nil {
+		t.Error("want error buying from zero copies, got nil")
 	}
 }
