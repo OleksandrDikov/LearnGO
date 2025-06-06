dfdb8fa46a28571b0043d9f0a18a6f48ed8f6aa1
OleksandrDikov <o.dikov@luxnet.io>
2025-06-06 12:56:52 +0300

    Update main.go

--- LearningGoBook/ch2/main.go
+++ LearningGoBook/ch2/main.go
@@ -8,4 +8,13 @@ func main() {
 	var i int = value
 	var f float64 = value
 	fmt.Println(i, f)
+
+	var b byte = 255
+	var smallI int32 = 2147483647
+	var bigI uint64 = 18446744073709551615
+	fmt.Println(b, smallI, bigI)
+
+	fmt.Println(b + 1)
+	fmt.Println(smallI + 1)
+	fmt.Println(bigI + 1)
 }
